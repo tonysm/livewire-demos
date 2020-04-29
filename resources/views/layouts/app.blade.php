76ae836377af5b6995463f7f0f4d67527c00b9ec
Tony Messias <tonyzrp@gmail.com>
2020-04-29 20:55:47 -0300

    Install livewire

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <livewire:styles />
 </head>
 <body>
     <div id="app">
@@ -76,5 +77,6 @@
             @yield('content')
         </main>
     </div>
+    <livewire:scripts />
 </body>
 </html>
