cd5b4b4a91111e9783b4264c7b9c716fde3aa081
Tony Messias <tonyzrp@gmail.com>
2020-04-29 21:49:02 -0300

    Adds the purchase button

--- app/Http/Livewire/Licenses/SeatsCalculator.php
+++ app/Http/Livewire/Licenses/SeatsCalculator.php
@@ -21,28 +21,46 @@ class SeatsCalculator extends Component
         $this->amount = SeatsPricesCalculator::calculate($this->seats);
     }
 
+    public function purchase()
+    {
+        sleep(4);
+        return redirect()->to('/');
+    }
+
     public function render()
     {
         return <<<'blade'
         <div class="w-25 mx-auto">
             <h1 class="text-center">Team License</h1>
             
-            <div class="card">
-                <div class="card-body">
-                    <p class="text-center">You license is currently for {{ trans_choice('{0} no seats|{1} one seat|[2,*] :count seats', $seats) }}</p>
-                    <div class="form-group">
-                        <label for="formControlRange" class="sr-only">Licenses</label>
-                        <input
-                            type="range"
-                            max="100"
-                            min="0"
-                            class="form-control-range"
-                            id="formControlRange"
-                            wire:model="seats"
-                        />
+            <form wire:submit.prevent="purchase">
+                <div class="card">
+                    <div class="card-body">
+                        <p class="text-center">You license is currently for {{ trans_choice('{0} no seats|{1} one seat|[2,*] :count seats', $seats) }}</p>
+                        <div class="form-group">
+                            <label for="formControlRange" class="sr-only">Licenses</label>
+                            <input
+                                type="range"
+                                name="seats"
+                                max="10"
+                                min="0"
+                                class="form-control-range"
+                                id="formControlRange"
+                                wire:model="seats"
+                            />
+                        </div>
+                        <h2 class="font-bold text-center">${{ number_format($amount, 2) }}</h2>
+                        <div class="form-group">
+                            <button
+                                class="btn btn-lg btn-block btn-primary"
+                                wire:loading.attr="disabled"
+                                wire:target="purchase"
+                            >
+                                Buy licenses!
+                            </button>
+                        </div>
                     </div>
-                    <h2 class="font-bold text-center">${{ number_format($amount, 2) }}</h2>
-                </div>
+                </form>
             </div>
         </div>
         blade;
