5d8a54dca79f3db67c6dbdd0fe36656c9c2ddd9a
Tony Messias <tonyzrp@gmail.com>
2020-05-13 21:14:46 -0300

    Adds boat seeds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+         factory(\App\Boat::class)->times(50)->create();
     }
 }
